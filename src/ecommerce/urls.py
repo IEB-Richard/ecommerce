5a7a33b824450f99d350aea7638fb475bf4db089
xiaoqiang.liu <xiaoqiang.liu@yahoo.com>
2019-10-18 23:48:11 +0800

    Section 30 - SlugField and Signals

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -22,6 +22,7 @@ from products.views import (
     ProductListView,
     product_list_view,
     ProductDetailView,
+    ProductDetailSlugView,
     product_detail_view,
     ProductFeaturedListView,
     ProductFeaturedDetailView
@@ -38,6 +39,7 @@ urlpatterns = [
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),
     path('products/<int:pk>/', ProductDetailView.as_view()),
+    path('products/<slug:slug>/', ProductDetailSlugView.as_view()),
     path('products-fbv/<int:pk>/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -14,12 +14,14 @@ class ProductFeaturedListView(ListView):
         request = self.request
         return Product.objects.all().featured()
 
+
 class ProductFeaturedDetailView(DetailView):
     template_name = "products/featured-detail.html"
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
         return Product.objects.all().featured()
+
 
 class ProductListView(ListView):
     queryset = Product.objects.all()
@@ -42,6 +44,26 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/list.html", context)
+
+
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+        # instance = get_object_or_404(Product, slug=slug)
+        try:
+            instance = Product.objects.get(slug=slug, active=True)
+        except Product.DoesNotExist:
+            raise Http404("Not found.")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug, active=True)
+            instance = qs.first()
+        except:
+            raise Http404("Uhhmmm ")
+        return instance
 
 
 class ProductDetailView(DetailView):
